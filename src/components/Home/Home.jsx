abc2af7979c745cccd3859847c3b15addba433f9
Rifat0001 <mahmudrifat764@gmail.com>
2023-05-02 19:18:41 +0600

    fetching data

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,22 @@
 // import React from 'react';
+import { useEffect, useState } from 'react';
 import Blogs from '../Blogs/Blogs';
 import Carts from '../Carts/Carts';
 
 const Home = () => {
+    const [blogs, setBlogs] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
     return (
         <div className="grid grid-cols-3 gap-4 px-16">
-            <Blogs></Blogs>
+            <Blogs>
+                {
+                    blogs.map(blog => <h1 key={blog.id}>hi</h1>)
+                }
+            </Blogs>
             <Carts></Carts>
         </div>
     );
