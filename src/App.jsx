f55c2fceaa5afa7cb2a019d6f8b99c24e4aaecb9
Rifat0001 <mahmudrifat764@gmail.com>
2023-05-03 14:37:47 +0600

    faq added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import React from 'react';
 
+import Faq from "./components/Faq/Faq";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <Header></Header>
       <Home></Home>
+      <Faq></Faq>
     </div>
   );
 };
---
+++ src/components/Faq/Faq.jsx
@@ -0,0 +1,58 @@
+// import React from 'react';
+
+const Faq = () => {
+    return (
+        <div className="bg-blue-100 py-4 md:mt-0 mt-4">
+            <h2 className="font-bold md:text-3xl text-2xl text-center ">Frequently Asked Questions</h2>
+            <div className="md:px-20 px-2 md:mt-4 mt-2">
+                <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+                    <div className="collapse-title text-lg font-medium">
+                        1. Props vs state ?
+                    </div>
+                    <div className="collapse-content">
+                        <p>Answer: Props is short for properties and they are used to pass data between React components. React’s data flow between components is uni-directional.</p>
+                        <p>React has another special built-in object called state, which allows components to create and manage their own data. So unlike props, components cannot pass data with state, but they can create and manage it internally.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="md:px-20 px-2 md:mt-4 mt-2">
+                <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+                    <div className="collapse-title text-lg font-medium">
+                        2.How does useState work?
+                    </div>
+                    <div className="collapse-content">
+                        <p>Answer: useState gives an array consists of two values: a variable and a set function which update the state variable. The initial value of useState can be empty array, 0, null or any initial value.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="md:px-20 px-2 md:mt-4 mt-2">
+                <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+                    <div className="collapse-title text-lg font-medium">
+                        3.Purpose of useEffect other than fetching data?
+                    </div>
+                    <div className="collapse-content">
+                        <p>Answer: Purpose of useEffect other than fetching data are the following </p>
+                        <ol>
+                            <li>1.Input field validation</li>
+                            <li>2.Side Effect Runs After Props and State Value Change</li>
+                            <li>3.Side Effect Runs Only Once After Initial Render</li>
+                            <li>4.Live filtering on state change.</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <div className="md:px-20 px-2 md:mt-4 mt-2">
+                <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+                    <div className="collapse-title text-lg font-medium">
+                        4.How Does React work?
+                    </div>
+                    <div className="collapse-content">
+                        <p>Answer: React is a JavaScript library developed by FaceBook. It is used for building interactive user interface and writing code using React is very efficient. It has some key features like JSX, Virtual DOM, Props, State Management etc. Besides, it is easy to use, easier component writing, high performance, SEO friendly etc. Whenever a React component returns an element, the Virtual DOM will update the real DOM to match. In this way react work efficiently.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Faq;
